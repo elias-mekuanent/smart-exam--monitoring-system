58ccf8affdc50cf410f3265adc3b214f34d3617d
Elias mekuanent <eliasnmnm@gmail.com>
2024-02-18 06:27:16 -0500

    notification-service

---
+++ notification-service/src/main/java/com/senpare/NotificationServiceApplication.java
@@ -0,0 +1,9 @@
+package com.senpare;
+
+import org.springframework.boot.SpringApplication;
+
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
